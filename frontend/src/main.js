0545fd1ef2b72d7c0078fe2fc9cd5ebb9400bf37
Loick Hommet <hommet.loick@gmail.com>
2024-07-18 16:45:52 +0200

    cors

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // Assurez-vous d'importer votre configuration de router correctement
+import axios from 'axios';
+
 
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
+
+axios.defaults.baseURL = 'http://localhost/api/register'; 
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS';
+axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
