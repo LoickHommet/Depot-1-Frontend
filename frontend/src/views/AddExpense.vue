2f101029d2d18f815f8ace3bd5935eac3e2acf7a
Loick Hommet <hommet.loick@gmail.com>
2024-07-19 12:06:35 +0200

    Creation de la view expenses

---
+++ frontend/src/views/AddExpense.vue
@@ -0,0 +1,22 @@
+<script setup>
+import ExpenseForm from '../components/ExpenseForm.vue';
+</script>
+
+<style scoped>
+.title {
+    text-align: center;
+    margin-bottom: 2rem;
+}
+</style>
+
+
+<template>
+    <main class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-6 col-md-8">
+                <h1 class="title mb-0 my-5">Ajouter une Dépense</h1>
+                <ExpenseForm />
+            </div>
+        </div>
+    </main>
+</template>
