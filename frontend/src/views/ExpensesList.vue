115bd2b40fd57ef9dbb48f17916affea9d6824c4
Loick Hommet <hommet.loick@gmail.com>
2024-07-19 14:41:21 +0200

    Creation de la view List

---
+++ frontend/src/views/ExpensesList.vue
@@ -0,0 +1,27 @@
+
+<script setup>
+import ExpensesList from '../components/ExpensesList.vue';
+</script>
+
+
+
+<template>
+  <div class="expenses-list-page">
+    <h1>Liste des Dépenses</h1>
+    <ExpensesList />
+  </div>
+</template>
+
+
+
+<style scoped>
+.expenses-list-page {
+  padding: 20px;
+}
+
+h1 {
+  margin-bottom: 20px;
+  font-size: 2em;
+  font-weight: bold;
+}
+</style>
